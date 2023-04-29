f3e8bc3b5fc1d5fa1aaf92afa559eb6d8f719c7a
emarkees <emarkees.com>
2023-04-29 01:50:27 -0600

    Add POST METHOD  function Api files

--- src/modules/Api.js
+++ src/modules/Api.js
@@ -19,4 +19,44 @@ export const getData = async () => {
 
 getData();
 
+const deployScore = async () => {
+  container.innerHTML = '';
+  const scoreObj = await getData();
+  const { result } = await scoreObj;
+ /* name.sort((a, b) => a - b); */
+  result.forEach((item) => {
+    container.innerHTML +=`
+    <ul class="contentlist">
+    <span class="board">${item.user}</span>
+    <span class="semi-column">: </span>
+    <span class="board">${item.score} </span>
+    </ul>
+    `
+  })
+};
+
+deployScore();
+
+//post a data 
+const postData = async(data) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data),
+      
+    });
+    const result = await response.json();
+    console.log("success", result)
+  }
+  catch (error) {
+    console.error(error);
+    throw new Error('Failed to post data');
+  }
+}
+postData();
+
+
 
